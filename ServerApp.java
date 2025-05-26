54549780dc040addc7f84141e1289534194f195f
Lyprandos Liatsos <102907631+LyprandosLia@users.noreply.github.com>
2025-05-26 11:03:39 +0300

    Add ClientHandler for multithreading

--- ServerApp.java
+++ ServerApp.java
@@ -24,22 +24,58 @@ public class ServerApp implements Runnable {
             File[] video_list = new File("video/").listFiles();
 
             while (true) {
-                //server_log.append("Listening for requests...\n");
-                System.out.println("Listening for requests...");
+                System.out.println("Listening for requests..");
                 Socket socket = server.accept();
-                 System.out.println("Client connected from " + socket.getInetAddress().getHostAddress() + ":" + socket.getPort());
+                System.out.println("Client connected from " + socket.getInetAddress().getHostAddress() + ":" + socket.getPort());
 
-                ObjectInputStream input_stream = new ObjectInputStream(socket.getInputStream());
-                ObjectOutputStream output_stream = new ObjectOutputStream(socket.getOutputStream());
+                //Handling each client as different socket
+                Thread clientThread = new Thread(new ClientHandler(socket, video_list));
+                clientThread.start();
+            }
+        } catch (IOException ex) {
+            java.util.logging.Logger.getLogger(ServerApp.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void run() {
+        try{
+            startServer();    
+        }catch (Exception e){
+            e.printStackTrace();
+        } 
+}
+    public static void main(String[] args){
+            ServerApp serverApp = new ServerApp();
+            Thread serverThread = new Thread(serverApp);
+            serverThread.start();
+    }
+
+    private static class ClientHandler implements Runnable{
+           
+        private final Socket clientSocket;
+        private final File[] videoList;
+        
+        public ClientHandler(Socket socket, File[] videoList){
+            this.clientSocket = socket;
+            this.videoList = videoList;
+        }
+          @Override
+        public void run() {
+            try {
+                ObjectInputStream input_stream = new ObjectInputStream(clientSocket.getInputStream());
+                ObjectOutputStream output_stream = new ObjectOutputStream(clientSocket.getOutputStream());
 
                 ArrayList<String> received_request = (ArrayList<String>) input_stream.readObject();
                 float maxResolution = Integer.parseInt(received_request.get(0));
                 String selected_format = received_request.get(1);
-                server_log.append("Received request for " + maxResolution + " resolution and " + selected_format + " format, ");
+
+                if (server_log != null) {
+                    server_log.append("Received request for " + maxResolution + " resolution and " + selected_format + " format\n");
+                }
 
                 ArrayList<String> available_videos = new ArrayList<>();
 
-                for (File video : video_list) {
+                for (File video : videoList) {
                     String current_video = video.getName();
 
                     if (current_video.endsWith("." + selected_format)) {
@@ -47,95 +83,75 @@ public class ServerApp implements Runnable {
 
                         if (parts.length >= 2) {
                             try {
-
-                                
-
-                                String resolutionPart = parts[1].replaceAll("[^0-9]","");
+                                String resolutionPart = parts[1].replaceAll("[^0-9]", "");
                                 int videoResolution = Integer.parseInt(resolutionPart);
 
                                 if (videoResolution <= maxResolution) {
                                     available_videos.add(current_video);
                                 }
-
                             } catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
                                 log.warn("Skipping malformed filename: " + current_video);
                             }
                         }
                     }
                 }
-               output_stream.writeObject(available_videos);
-               ArrayList<String> stream_specs = (ArrayList<String>)input_stream.readObject();
-               String selected_video = stream_specs.get(0);
-               String selected_protocol = stream_specs.get(1);
-               
-               server_log.append("using the " + selected_protocol + " protocol to stream '" + selected_video + "'\n\n");
-               String videos_dir_fullpath = System.getProperty("user.dir") + "/videos";
-               
-               ArrayList<String> command_line_args = new ArrayList<>();
-               command_line_args.add("ffmpeg");
-               
-               switch (selected_protocol){
-                   case "UDP":
-                       command_line_args.add("-re");
-			command_line_args.add("-i");
-			command_line_args.add(videos_dir_fullpath + "/" + selected_video);
-			command_line_args.add("-f");
-			command_line_args.add("mpegts");
-			command_line_args.add("udp://127.0.0.1:6000");
+
+                output_stream.writeObject(available_videos);
+
+                ArrayList<String> stream_specs = (ArrayList<String>) input_stream.readObject();
+                String selected_video = stream_specs.get(0);
+                String selected_protocol = stream_specs.get(1);
+
+                if (server_log != null) {
+                    server_log.append("Using " + selected_protocol + " to stream '" + selected_video + "'\n\n");
+                }
+
+                String videos_dir_fullpath = System.getProperty("user.dir") + "/videos";
+                ArrayList<String> command_line_args = new ArrayList<>();
+                command_line_args.add("ffmpeg");
+
+                switch (selected_protocol) {
+                    case "UDP":
+                        command_line_args.add("-re");
+                        command_line_args.add("-i");
+                        command_line_args.add(videos_dir_fullpath + "/" + selected_video);
+                        command_line_args.add("-f");
+                        command_line_args.add("mpegts");
+                        command_line_args.add("udp://127.0.0.1:6000");
                         break;
-                   case "TCP":   
-                       command_line_args.add("-i");
-			command_line_args.add(videos_dir_fullpath + "/" + selected_video);
-			command_line_args.add("-f");
-			command_line_args.add("mpegts");
-			command_line_args.add("tcp://127.0.0.1:5100?listen");
+                    case "TCP":
+                        command_line_args.add("-i");
+                        command_line_args.add(videos_dir_fullpath + "/" + selected_video);
+                        command_line_args.add("-f");
+                        command_line_args.add("mpegts");
+                        command_line_args.add("tcp://127.0.0.1:5100?listen");
                         break;
-                   case "RTP":
-                       command_line_args.add("-re");
-			command_line_args.add("-i");
-			command_line_args.add(videos_dir_fullpath + "/" + selected_video);
-			command_line_args.add("-an");
-			command_line_args.add("-c:v");
-			command_line_args.add("copy");
-			command_line_args.add("-f");
-			command_line_args.add("rtp");
-			command_line_args.add("-sdp_file");
-			command_line_args.add(System.getProperty("user.dir") + "/video.sdp");
-			command_line_args.add("rtp://127.0.0.1:5004?rtcpport=5008");   
+                    case "RTP":
+                        command_line_args.add("-re");
+                        command_line_args.add("-i");
+                        command_line_args.add(videos_dir_fullpath + "/" + selected_video);
+                        command_line_args.add("-an");
+                        command_line_args.add("-c:v");
+                        command_line_args.add("copy");
+                        command_line_args.add("-f");
+                        command_line_args.add("rtp");
+                        command_line_args.add("-sdp_file");
+                        command_line_args.add(System.getProperty("user.dir") + "/video.sdp");
+                        command_line_args.add("rtp://127.0.0.1:5004?rtcpport=5008");
                         break;
+                }
+
+                ProcessBuilder process_builder = new ProcessBuilder(command_line_args);
+                process_builder.inheritIO().start();
+
+                output_stream.close();
+                input_stream.close();
+                clientSocket.close();
+
+            } catch (IOException | ClassNotFoundException e) {
+                log.error("Error in client handler: ", e);
             }
-               ProcessBuilder process_builder = new ProcessBuilder(command_line_args);
-			Process streamer_host = process_builder.start();
-			
-			output_stream.close();
-			input_stream.close();
-			socket.close();
-            }
-        
-
-
-
-        } catch (IOException ex) {
-            java.util.logging.Logger.getLogger(ServerApp.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
-
-    @Override
-    public void run() {
-    try {
-        startServer();
-    } catch (Exception e) {
-        e.printStackTrace();
-    }
 }
     
-    public static void main(String[] args) {
-        ServerApp serverApp = new ServerApp();
-        Thread serverThread = new Thread(serverApp);
-        serverThread.start();
-    }
-}
-
-
-   
