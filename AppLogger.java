689ac982906384576b370f8a861cbeb929c6aed3
Lyprandos Liatsos <102907631+LyprandosLia@users.noreply.github.com>
2025-06-10 12:47:42 +0300

    added more comments

--- AppLogger.java
+++ AppLogger.java
@@ -10,38 +10,42 @@ import java.util.logging.*;
  * @author Lyprandos
  */
 public class AppLogger {
+        //added basic logger
         private static final Logger logger = Logger.getLogger("PolumesaLogger");
         //added staticis logger
         private static final Logger statsLogger = Logger.getLogger("StatsLogger");
     static{
        try{
-           //loggs
-           logger.setLevel(Level.INFO);
-           
+           //Initializing each logger
+           logger.setLevel(Level.INFO); //this logger will contain info
+           //Console handler to keep track what is printed in console
            ConsoleHandler ch = new ConsoleHandler();
-           ch.setLevel(Level.INFO);
-           logger.addHandler(ch);
+           ch.setLevel(Level.INFO); 
+           logger.addHandler(ch); //add the handler to logger
            
-           FileHandler fh = new FileHandler("polymesa.log", true);
+           //File handler for writing to a log file
+           FileHandler fh = new FileHandler("polymesa.log", true); //true means append
            fh.setLevel(Level.INFO);
-           fh.setFormatter(new SimpleFormatter());
-           logger.addHandler(fh);
+           fh.setFormatter(new SimpleFormatter()); //Using simple formatter for reading
+           logger.addHandler(fh); //add filehandler to logger
+           
            
            //stats logger
+           FileHandler statsHandler = new FileHandler("streaming_stats.log",true); //append mode
+           statsHandler.setLevel(Level.INFO); 
+           statsHandler.setFormatter(new SimpleFormatter()); //formatter for reading
+           statsLogger.addHandler(statsHandler); //add filehandler to the stats logger
            
-           FileHandler statsHandler = new FileHandler("streaming_stats.log",true);
-           statsHandler.setLevel(Level.INFO);
-           statsLogger.addHandler(statsHandler);
-           statsHandler.setFormatter(new SimpleFormatter());
            
            
 
 
     }catch(IOException e){
+        //Track tracing in case of error initializing loggers
     System.err.println("Failed to initialize logger " + e.getMessage());
 }
     
-}
+}   //Getters of each logger (the are used in ServerApp and ClientApp)
     public static Logger getLogger(){
         return logger;
     }
